b44af1d2416e7347b40e3938e2cf862eb797df0f
PatoSalazar <psalazar@nascentdigital.com>
2019-01-04 12:18:42 -0500

    added custom lineChart to demo app

--- MyTelusGraphDemo/ViewControllers/CustomGraphsViewController.swift
+++ MyTelusGraphDemo/ViewControllers/CustomGraphsViewController.swift
@@ -6,17 +6,29 @@ internal class CustomGraphsViewController: UIViewController {
     // MARK: Properties
     
     // MARK: IBOutlets
+    @IBOutlet private weak var lineGraph: ChartView!
     
     // MARK: Life Cycle
     
     internal override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor.magenta
+        initializeLineGraph()
+        
     }
     
     // MARK: Internal Methods
     
     // MARK: Private Methods
     
+    private func initializeLineGraph() {
+        
+        let horizontalAxisConfig = LineChartAxisConfig(minValue: 2, maxValue: 14, numberOfSegments: 2, unitOfMeasure: nil, title: "Data")
+        
+        let verticalAxisConfig = LineChartAxisConfig(minValue: 0, maxValue: 14, numberOfSegments: 2, unitOfMeasure: "GB", title: "Days")
+        
+        let lineChart = LineChart(xConfig: horizontalAxisConfig, yConfig: verticalAxisConfig)
+        
+        lineGraph.initializeLineChart(lineChart: lineChart, gridType: .horizontal)
+    }
 }
