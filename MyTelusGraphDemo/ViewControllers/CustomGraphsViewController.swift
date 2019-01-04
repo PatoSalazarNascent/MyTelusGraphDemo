8f3ec614b5d6e05cd2e8b58b4a46cf98099da0c9
PatoSalazar <psalazar@nascentdigital.com>
2019-01-04 11:35:03 -0500

    added tab system for demo purposes

---
+++ MyTelusGraphDemo/ViewControllers/CustomGraphsViewController.swift
@@ -0,0 +1,22 @@
+import Foundation
+import UIKit
+
+internal class CustomGraphsViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    // MARK: IBOutlets
+    
+    // MARK: Life Cycle
+    
+    internal override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.magenta
+    }
+    
+    // MARK: Internal Methods
+    
+    // MARK: Private Methods
+    
+}
---
+++ MyTelusGraphDemo/ViewControllers/LibraryGraphsViewController.swift
@@ -0,0 +1,21 @@
+import Foundation
+import UIKit
+
+internal class LibraryGraphsViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    // MARK: IBOutlets
+    
+    // MARK: Life Cycle
+    
+    internal override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.purple
+    }
+    
+    // MARK: Internal Methods
+    
+    // MARK: Private Methods
+}
